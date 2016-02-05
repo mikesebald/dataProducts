f6519f43574cbc6791405c7f1ddd86b725a51864
mikesebald <michael.sebald@gmail.com>
2016-02-05 10:05:27 +0100

    commit

--- dataProducts.R
+++ dataProducts.R
@@ -4,4 +4,12 @@ library(shiny)
 library(manipulate)
 library(rCharts)
 library(googleVis)
+library(plotly)
 
+library(manipulate)
+myPlot <- function(s) {
+  plot(cars$dist - mean(cars$dist), cars$speed - mean(cars$speed))
+  abline(0, s)
+}
+
+manipulate(myPlot(s), s = slider(0, 2, step = 0.1))
